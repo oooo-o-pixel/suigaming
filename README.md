077ece3aa31ce7eb12d32c4eedce7e57bc091cb1
triumphsta <nzefilitriumph@gmail.com>
2026-01-30 10:14:10 +0100

    i edited core views and added slides

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # suigaming
 A gaming website for web2 and web3 tournaments
+
+
+please i made some new models in core is for the sliders and if i need it anywhere i'll use template inheritance
--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import EventSlide
+
 
 # Register your models here.
+admin.site.site_header = "SuiGaming Admin"
+admin.site.site_title = "SuiGaming Admin Portal"
+admin.site.index_title = "Welcome to SuiGaming Admin Portal"
+
+
+admin.site.register(EventSlide)
